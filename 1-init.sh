a38318f1e48681f9feb074e6d67b5e47bc7b8923
Martin Jansa <martin.jansa@lge.com>
2020-02-08 16:54:59 -0800

    1-init: add qtwebkit chromium firefox epiphany
    * insert evil laugh here
    * I've realized that even epiphany with its webkitgtk isn't included
      in the image we're building - core-image-sato, since midory was
      replaced with epiphany here:
      https://github.com/openembedded/openembedded-core/commit/f1c9a0d486e18dd8b2b1da9525f7b53616572112#diff-75f8120b29b302194998da986c00e785R50
      so the image was much "smaller" than expected
    * with 5 different webengines included now, it should be more than
      enough for everybody, but lets see if
      3990X: One CPU to rule them all in 2020 ...
    * add *.top20 files with 20 longest running tasks (just to see if
      different builders were strugling with the same set of tasks)
    
    Signed-off-by: Martin Jansa <martin.jansa@lge.com>

--- 1-init.sh
+++ 1-init.sh
@@ -11,11 +11,31 @@ git clone https://github.com/meta-qt5/meta-qt5.git
 cd meta-qt5
 git checkout -b zeus a582fd4c810529e9af0c81700407b1955d1391d2
 cd ..
+git clone https://github.com/OSSystems/meta-browser.git
+cd meta-browser
+git checkout -b zeus 1697014a05967ed54b294f3c4b5621df494d6551
+cd ..
+git clone https://github.com/openembedded/meta-openembedded.git
+cd meta-openembedded
+git checkout -b zeus e855ecc6d35677e79780adc57b2552213c995731
+cd ..
+git clone https://github.com/kraj/meta-clang.git
+cd meta-clang
+git checkout -b zeus 0c393398a91713a108f319ac75337c02b7ebeaa7
+cd ..
+git clone https://github.com/meta-rust/meta-rust.git
+cd meta-rust
+git checkout -b zeus d8d77be1292064a02adcb5e72e293604b704f69b
+cd ..
 
 . ./oe-init-build-env
 sed -i 's/^\(.*\)meta-yocto-bsp/\1meta-yocto-bsp \\\n\1meta-qt5/g' conf/bblayers.conf
+sed -i 's/^\(.*\)meta-yocto-bsp/\1meta-yocto-bsp \\\n\1meta-clang/g' conf/bblayers.conf
+sed -i 's/^\(.*\)meta-yocto-bsp/\1meta-yocto-bsp \\\n\1meta-openembedded\/meta-oe/g' conf/bblayers.conf
+sed -i 's/^\(.*\)meta-yocto-bsp/\1meta-yocto-bsp \\\n\1meta-rust/g' conf/bblayers.conf
+sed -i 's/^\(.*\)meta-yocto-bsp/\1meta-yocto-bsp \\\n\1meta-browser/g' conf/bblayers.conf
 
 cat >> conf/local.conf << EOF
-IMAGE_INSTALL_append_pn-core-image-sato = " qtwebengine"
+IMAGE_INSTALL_append_pn-core-image-sato = " qtwebengine qtwebkit chromium-x11 firefox epiphany"
 MACHINE = "qemux86-64"
 EOF
