144bbd1dc925128eaea3b0644f44a917dac1a4b7
Martin Jansa <Martin.Jansa@gmail.com>
2020-05-02 00:45:07 +0200

    8-build-individual-components.sh: add one more run with 3/4 of available threads
    Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

--- 8-build-individual-components.sh
+++ 8-build-individual-components.sh
@@ -34,3 +34,19 @@ for i in ${COMPONENTS}; do
     bitbake -c configure $i;
     /usr/bin/time -f "$TIME_STR" bitbake -c compile $i 2>&1 | tee ../../8-build-individual-components.${THREADS}.$i.log
 done
+
+# And this one might be interesting for 3990x with just 96 instead
+# of full 128 threads (if it still fits in 128G ram) to use more than
+# 64 threads shown in 3970x tests
+THREADS=`cat /proc/cpuinfo  | grep ^processor | wc -l`
+THREADS=`expr ${THREADS} - ${THREADS} / 4`
+cat >> conf/local.conf << EOF
+BB_NUMBER_THREADS = "8"
+PARALLEL_MAKE = "-j ${THREADS}"
+EOF
+
+for i in ${COMPONENTS}; do
+    bitbake -c cleansstate $i;
+    bitbake -c configure $i;
+    /usr/bin/time -f "$TIME_STR" bitbake -c compile $i 2>&1 | tee ../../8-build-individual-components.${THREADS}.$i.log
+done
