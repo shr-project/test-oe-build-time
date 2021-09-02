ad63567e430e1313c0c658b4a255e68e3d553b4b
Martin Jansa <Martin.Jansa@gmail.com>
2021-09-02 08:55:53 +0200

    1-init.sh: update for latest revisions from honister
    * remove separate meta-rust layer, rust is now integrated in oe-core,
      update meta-firefox layer not to depend on rust-layer
    * honister is being developed in master branches
    * zstd liblz4-tool are new host dependencies needed for honister
    * update overrides syntax
    
    Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

--- 0-sudo-init.sh
+++ 0-sudo-init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Needed for OE sanity
-apt-get install -y build-essential chrpath git locales lsb-release iputils-ping python python2.7 python3 texinfo cpio diffstat gawk wget python3-distutils bzr iproute2
+apt-get install -y build-essential chrpath git locales lsb-release iputils-ping python python2.7 python3 texinfo cpio diffstat gawk wget python3-distutils bzr iproute2 zstd liblz4-tool
 
 # Needed for test.sh
 apt-get install -y time
--- 1-init.sh
+++ 1-init.sh
@@ -6,30 +6,27 @@
 
 git clone git://git.yoctoproject.org/poky
 cd poky
-git checkout -b hardknott 3cda6c0bd175282903079dea4affb3fcd290f137
+git checkout -b honister b978f7c3a028ff4cc61de79d296723b5f873651d
 git clone https://github.com/meta-qt5/meta-qt5.git
 cd meta-qt5
-git checkout -b hardknott a00af3eae082b772469d9dd21b2371dd4d237684
+git checkout -b honister 2a38fca150f065f869ed530fffe1a07beec80692
 cd ..
 git clone https://github.com/OSSystems/meta-browser.git
 cd meta-browser
-git checkout -b hardknott bdde7ca1fed0e29441d94ad6af8319b6df48063a
+git checkout -b honister a7d78e7d86869c3e9c9dbcc5be8774f54a03e13a
+sed -i 's/LAYERDEPENDS_firefox-browser-layer = "clang-layer core openembedded-layer rust-layer"/LAYERDEPENDS_firefox-browser-layer = "clang-layer core openembedded-layer"/g' meta-firefox/conf/layer.conf
 cd ..
 git clone https://github.com/openembedded/meta-openembedded.git
 cd meta-openembedded
-git checkout -b hardknott 36bb7d02a0f189768b380747a2349406ceffacbf
+git checkout -b honister 967fe6730c6b0ab9fa8f350adef7bffea0aeb67b
 cd ..
 git clone https://github.com/kraj/meta-clang.git
 cd meta-clang
-git checkout -b hardknott e8a0bada69637ff7a767d387b803a42123aead0b
-cd ..
-git clone https://github.com/meta-rust/meta-rust.git
-cd meta-rust
-git checkout -b hardknott 9a035fe27262bb23676da9f9f583e3ee39b0a377
+git checkout -b honister 5187604e18b970e486b88e6f2eba6a454ef4f7ef
 cd ..
 git clone git://git.openembedded.org/meta-python2
 cd meta-python2
-git checkout -b hardknott 5f4db648e00cf1c41e0999d3b184a02cde225143
+git checkout -b honister f522fa70f93a9339f3b9991082af69a09fde675a
 cd ..
 
 . ./oe-init-build-env
@@ -42,9 +39,6 @@ fi
 if ! grep -q meta-oe conf/bblayers.conf ; then
   sed -i 's/^\(.*\)meta-yocto-bsp/\1meta-yocto-bsp \\\n\1meta-openembedded\/meta-oe/g' conf/bblayers.conf
 fi
-if ! grep -q meta-rust conf/bblayers.conf ; then
-  sed -i 's/^\(.*\)meta-yocto-bsp/\1meta-yocto-bsp \\\n\1meta-rust/g' conf/bblayers.conf
-fi
 if ! grep -q meta-browser conf/bblayers.conf ; then
   sed -i 's/^\(.*\)meta-yocto-bsp/\1meta-yocto-bsp \\\n\1meta-browser\/meta-firefox/g' conf/bblayers.conf
   sed -i 's/^\(.*\)meta-yocto-bsp/\1meta-yocto-bsp \\\n\1meta-browser\/meta-chromium/g' conf/bblayers.conf
@@ -54,7 +48,7 @@ if ! grep -q meta-python2 conf/bblayers.conf ; then
 fi
 
 cat >> conf/local.conf << EOF
-IMAGE_INSTALL_append_pn-core-image-sato = " qtwebengine qtwebkit chromium-x11 firefox epiphany"
+IMAGE_INSTALL:append:pn-core-image-sato = " qtwebengine qtwebkit chromium-x11 firefox epiphany"
 MACHINE = "qemux86-64"
 INHERIT += "rm_work"
 EOF
