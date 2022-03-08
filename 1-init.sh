31cee1b02cbaa97c7c861b11ae713b0bd2993f56
Martin Jansa <Martin.Jansa@gmail.com>
2022-03-08 11:39:01 +0100

    1-init.sh: update for latest revisions for kirkstone
    * kirkstone is being developed in master branches, use latest revisions as of today
    
    Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

--- 1-init.sh
+++ 1-init.sh
@@ -6,26 +6,35 @@
 
 git clone git://git.yoctoproject.org/poky
 cd poky
-git checkout -b honister 0fbf414b39f1ce3a055c5baeba755a05c438ec0d
+git checkout -b kirkstone 8a4c53ee762c1ee02ba282d265e12e733c81ce82
+# to fix libsdl2-native build on gentoo with libunwind installed
+git cherry-pick 0d6b4a93e58f72e17fe0839d3f337efafded1277
 git clone https://github.com/meta-qt5/meta-qt5.git
 cd meta-qt5
-git checkout -b honister 74c27fa59b329f1210d39e8cd2904acf30d10838
+git checkout -b kirkstone 4837db1d965c51e4a2c5971e6a126c896e5b401a
 cd ..
 git clone https://github.com/OSSystems/meta-browser.git
 cd meta-browser
-git checkout -b honister ccbdc01df693d675d75d01ab798cc5c377466ac2
+git checkout -b kirkstone ccbdc01df693d675d75d01ab798cc5c377466ac2
+# to fix firefox build after:
+# https://git.openembedded.org/openembedded-core/commit/meta/classes/rust-common.bbclass?id=997d54363a3cb3a0e949b3626855f2fa41afeb2b
+wget https://github.com/OSSystems/meta-browser/commit/df743dd5e9a660b64e750800747e68445914ee8f.patch
+git am df743dd5e9a660b64e750800747e68445914ee8f.patch
+# and with wayland-1.20
+wget https://github.com/OSSystems/meta-browser/commit/dfeb4defc8b939714ccce797c30245ae4b3ede93.patch
+git am dfeb4defc8b939714ccce797c30245ae4b3ede93.patch
 cd ..
 git clone https://github.com/openembedded/meta-openembedded.git
 cd meta-openembedded
-git checkout -b honister 0fb490a08ce30b47a5ccd2fdc3448b08e0d9e4e9
+git checkout -b kirkstone a3f218e9db0caeb544d834a0f3f374aac7d0e4f3
 cd ..
 git clone https://github.com/kraj/meta-clang.git
 cd meta-clang
-git checkout -b honister 088904d40231d9e099c2f5039cd3c2bc47d332d1
+git checkout -b kirkstone 8efb230dd73cfd19e575ff42fea979d977b05c97
 cd ..
 git clone git://git.openembedded.org/meta-python2
 cd meta-python2
-git checkout -b honister 5243d509aebff378c1ae9d3dff6a29cfdc0dee1f
+git checkout -b kirkstone f02882e2aa9279ca7becca8d0cedbffe88b5a253
 cd ..
 
 . ./oe-init-build-env
