1d7e26ad5ef8d4178d579f2c42be7fbd3dafda68
Martin Jansa <Martin.Jansa@gmail.com>
2022-05-16 13:55:39 +0200

    1-init.sh: update for latest revisions for langdale
    * langdale is being developed in master branches, use latest revisions
      as of today
    
    Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

--- 1-init.sh
+++ 1-init.sh
@@ -6,35 +6,29 @@
 
 git clone git://git.yoctoproject.org/poky
 cd poky
-git checkout -b kirkstone 8a4c53ee762c1ee02ba282d265e12e733c81ce82
-# to fix libsdl2-native build on gentoo with libunwind installed
-git cherry-pick 0d6b4a93e58f72e17fe0839d3f337efafded1277
+git checkout -b langdale bd66a18eaa463c2eab291fea68039bf0d61c7b8b
 git clone https://github.com/meta-qt5/meta-qt5.git
 cd meta-qt5
-git checkout -b kirkstone 4837db1d965c51e4a2c5971e6a126c896e5b401a
+git checkout -b langdale 6b29b21775558eea1a3ef3007133f08849b61b00
 cd ..
 git clone https://github.com/OSSystems/meta-browser.git
 cd meta-browser
-git checkout -b kirkstone ccbdc01df693d675d75d01ab798cc5c377466ac2
-# to fix firefox build after:
-# https://git.openembedded.org/openembedded-core/commit/meta/classes/rust-common.bbclass?id=997d54363a3cb3a0e949b3626855f2fa41afeb2b
-wget https://github.com/OSSystems/meta-browser/commit/df743dd5e9a660b64e750800747e68445914ee8f.patch
-git am df743dd5e9a660b64e750800747e68445914ee8f.patch
-# and with wayland-1.20
-wget https://github.com/OSSystems/meta-browser/commit/dfeb4defc8b939714ccce797c30245ae4b3ede93.patch
-git am dfeb4defc8b939714ccce797c30245ae4b3ede93.patch
+git checkout -b langdale 726f2e831c62a4fa894fcfd93c283065845c8b18
+# To fix build with rust-1.60 add pending patch from PR
+wget https://github.com/OSSystems/meta-browser/pull/638/commits/c4cc1810020ec2d395a718742bf05dbbc88fbc6d.patch
+git am c4cc1810020ec2d395a718742bf05dbbc88fbc6d.patch
 cd ..
 git clone https://github.com/openembedded/meta-openembedded.git
 cd meta-openembedded
-git checkout -b kirkstone a3f218e9db0caeb544d834a0f3f374aac7d0e4f3
+git checkout -b langdale 1fa8cfed01b3088dc33e0958e83f1162ffc1b91b
 cd ..
 git clone https://github.com/kraj/meta-clang.git
 cd meta-clang
-git checkout -b kirkstone 8efb230dd73cfd19e575ff42fea979d977b05c97
+git checkout -b langdale 85d956d95401479ca666139e31f662f60c156d5f
 cd ..
 git clone git://git.openembedded.org/meta-python2
 cd meta-python2
-git checkout -b kirkstone f02882e2aa9279ca7becca8d0cedbffe88b5a253
+git checkout -b langdale f02882e2aa9279ca7becca8d0cedbffe88b5a253
 cd ..
 
 . ./oe-init-build-env
