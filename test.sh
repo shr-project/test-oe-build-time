5d674c60f822fe9d97763df6d56e3ca5f05dae4f
Martin Jansa <Martin.Jansa@gmail.com>
2022-05-18 09:57:14 +0200

    test.sh: add 'date' before and after the build
    * to make it easier to screeshots the graphs from grafana

--- test.sh
+++ test.sh
@@ -1,29 +1,15 @@
 #!/bin/sh
 
+date > 1-init.log
 /usr/bin/time ./1-init.sh 2>&1 | tee 1-init.log
 
-rm -rf poky/build/bitbake-cookerdaemon.log poky/build/cache poky/build/sstate-cache poky/build/tmp
-/usr/bin/time ./2-build-test.sh 2>&1 | tee 2-build-test.log
-grep -R "Elapsed time" poky/build/tmp/buildstats | sed 's/^.*\/\([^\/]*\/[^\/]*\):Elapsed time: \(.*\)$/\2 \1/g' | sort -n | tail -n 20 | tee 2-build-test.top20
-
-rm -rf poky/build/bitbake-cookerdaemon.log poky/build/cache poky/build/sstate-cache poky/build/tmp
-/usr/bin/time ./3-fetch.sh 2>&1 | tee 3-fetch.log
-grep -R "Elapsed time" poky/build/tmp/buildstats | sed 's/^.*\/\([^\/]*\/[^\/]*\):Elapsed time: \(.*\)$/\2 \1/g' | sort -n | tail -n 20 | tee 3-fetch.top20
-
-rm -rf poky/build/bitbake-cookerdaemon.log poky/build/cache poky/build/sstate-cache poky/build/tmp
-/usr/bin/time ./4-build-all-cores.sh 2>&1 | tee 4-build-all-cores.log
-grep -R "Elapsed time" poky/build/tmp/buildstats | sed 's/^.*\/\([^\/]*\/[^\/]*\):Elapsed time: \(.*\)$/\2 \1/g' | sort -n | tail -n 20 | tee 4-build-all-cores.top20
-
-rm -rf poky/build/bitbake-cookerdaemon.log poky/build/cache poky/build/sstate-cache poky/build/tmp
-/usr/bin/time ./5-build-8-bb-threads.sh 2>&1 | tee 5-build-8-bb-threads.log
-grep -R "Elapsed time" poky/build/tmp/buildstats | sed 's/^.*\/\([^\/]*\/[^\/]*\):Elapsed time: \(.*\)$/\2 \1/g' | sort -n | tail -n 20 | tee 5-build-8-bb-threads.top20
-
-rm -rf poky/build/bitbake-cookerdaemon.log poky/build/cache poky/build/sstate-cache poky/build/tmp
-/usr/bin/time ./6-build-16-bb-threads.sh 2>&1 | tee 6-build-16-bb-threads.log
-grep -R "Elapsed time" poky/build/tmp/buildstats | sed 's/^.*\/\([^\/]*\/[^\/]*\):Elapsed time: \(.*\)$/\2 \1/g' | sort -n | tail -n 20 | tee 6-build-16-bb-threads.top20
-
-rm -rf poky/build/bitbake-cookerdaemon.log poky/build/cache poky/build/sstate-cache poky/build/tmp
-/usr/bin/time ./7-build-2-bb-threads.sh 2>&1 | tee 7-build-2-bb-threads.log
-grep -R "Elapsed time" poky/build/tmp/buildstats | sed 's/^.*\/\([^\/]*\/[^\/]*\):Elapsed time: \(.*\)$/\2 \1/g' | sort -n | tail -n 20 | tee 7-build-2-bb-threads.top20
+for BUILD in 2-build-test 3-fetch 4-build-all-cores 5-build-8-bb-threads 6-build-16-bb-threads 7-build-2-bb-threads; do
+  rm -rf poky/build/bitbake-cookerdaemon.log poky/build/cache poky/build/sstate-cache poky/build/tmp
+  echo "${BUILD} start: `date`" | tee -a start-stop.log
+  LC_ALL=C date > ${BUILD}.log
+  /usr/bin/time ./${BUILD}.sh 2>&1 | tee ${BUILD}.log
+  echo "${BUILD} stop: `date`" | tee -a start-stop.log
+  grep -R "Elapsed time" poky/build/tmp/buildstats | sed 's/^.*\/\([^\/]*\/[^\/]*\):Elapsed time: \(.*\)$/\2 \1/g' | sort -n | tail -n 20 | tee ${BUILD}.top20
+done
 
 tail -n 5 *.log
