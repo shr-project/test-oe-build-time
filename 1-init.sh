949207c812c7974a1c18c11e3ba06233f8bb27dc
Martin Jansa <Martin.Jansa@gmail.com>
2022-07-29 13:29:43 +0200

    1-init.sh: update for latest revisions for kirkstone
    kirkstone was released a while ago, now it's 4.0.2
    
    Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

--- 1-init.sh
+++ 1-init.sh
@@ -6,31 +6,22 @@
 
 git clone git://git.yoctoproject.org/poky
 cd poky
-git checkout -b kirkstone 8a4c53ee762c1ee02ba282d265e12e733c81ce82
-# to fix libsdl2-native build on gentoo with libunwind installed
-git cherry-pick 0d6b4a93e58f72e17fe0839d3f337efafded1277
+git checkout -b kirkstone e4b5c35fd430e1aec8218b4ae4ab51b2b919eec6
 git clone https://github.com/meta-qt5/meta-qt5.git
 cd meta-qt5
-git checkout -b kirkstone 4837db1d965c51e4a2c5971e6a126c896e5b401a
+git checkout -b kirkstone 5b71df60e523423b9df6793de9387f87a149ac42
 cd ..
 git clone https://github.com/OSSystems/meta-browser.git
 cd meta-browser
-git checkout -b kirkstone ccbdc01df693d675d75d01ab798cc5c377466ac2
-# to fix firefox build after:
-# https://git.openembedded.org/openembedded-core/commit/meta/classes/rust-common.bbclass?id=997d54363a3cb3a0e949b3626855f2fa41afeb2b
-wget https://github.com/OSSystems/meta-browser/commit/df743dd5e9a660b64e750800747e68445914ee8f.patch
-git am df743dd5e9a660b64e750800747e68445914ee8f.patch
-# and with wayland-1.20
-wget https://github.com/OSSystems/meta-browser/commit/dfeb4defc8b939714ccce797c30245ae4b3ede93.patch
-git am dfeb4defc8b939714ccce797c30245ae4b3ede93.patch
+git checkout -b kirkstone 1f3ccca5678d7cbb221645227ec403aa767c16dd
 cd ..
 git clone https://github.com/openembedded/meta-openembedded.git
 cd meta-openembedded
-git checkout -b kirkstone a3f218e9db0caeb544d834a0f3f374aac7d0e4f3
+git checkout -b kirkstone a47ef046619d639dfbd3be2a13ef6d5b40fd40a1
 cd ..
 git clone https://github.com/kraj/meta-clang.git
 cd meta-clang
-git checkout -b kirkstone 8efb230dd73cfd19e575ff42fea979d977b05c97
+git checkout -b kirkstone d669d873edf68dc7440bb07096737203bb7ec505
 cd ..
 git clone git://git.openembedded.org/meta-python2
 cd meta-python2
