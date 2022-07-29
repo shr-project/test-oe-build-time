3df9d40b811078d8e83de34cd412aed6ea5d1072
Martin Jansa <Martin.Jansa@gmail.com>
2022-07-29 13:47:39 +0200

    test.sh: fix start-stop timestamps in log files
    Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

--- test.sh
+++ test.sh
@@ -2,15 +2,20 @@
 
 export LC_ALL=C
 
-date > 1-init.log
-/usr/bin/time ./1-init.sh 2>&1 | tee 1-init.log
+BUILD=1-init
+echo "${BUILD} start: `date`"    | tee    start-stop.log
+echo "${BUILD} start: `date`"    | tee    ${BUILD}.log
+/usr/bin/time ./${BUILD}.sh 2>&1 | tee -a ${BUILD}.log
+echo "${BUILD} stop: `date`"     | tee -a ${BUILD}.log
+echo "${BUILD} stop: `date`"     | tee -a start-stop.log
 
 for BUILD in 2-build-test 3-fetch 4-build-all-cores 5-build-8-bb-threads 6-build-16-bb-threads 7-build-2-bb-threads; do
   rm -rf poky/build/bitbake-cookerdaemon.log poky/build/cache poky/build/sstate-cache poky/build/tmp
-  echo "${BUILD} start: `date`" | tee -a start-stop.log
-  date > ${BUILD}.log
-  /usr/bin/time ./${BUILD}.sh 2>&1 | tee ${BUILD}.log
-  echo "${BUILD} stop: `date`" | tee -a start-stop.log
+  echo "${BUILD} start: `date`"    | tee -a start-stop.log
+  echo "${BUILD} start: `date`"    | tee    ${BUILD}.log
+  /usr/bin/time ./${BUILD}.sh 2>&1 | tee -a ${BUILD}.log
+  echo "${BUILD} stop: `date`"     | tee -a ${BUILD}.log
+  echo "${BUILD} stop: `date`"     | tee -a start-stop.log
   grep -R "Elapsed time" poky/build/tmp/buildstats | sed 's/^.*\/\([^\/]*\/[^\/]*\):Elapsed time: \(.*\)$/\2 \1/g' | sort -n | tail -n 20 | tee ${BUILD}.top20
 done
 
