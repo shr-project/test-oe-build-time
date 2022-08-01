2a51a39e6d1733de6033cf2cabfd7d7869c76daa
Martin Jansa <Martin.Jansa@gmail.com>
2022-08-01 12:04:54 +0200

    test.sh: use iso-8601 format for dates so that it's a bit easier to parse
    * e.g. when mapping start/stop times to grafana graphs
    
    Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

--- test.sh
+++ test.sh
@@ -18,21 +18,21 @@ else
 fi
 
 BUILD=1-init
-echo "${BUILD} start: `date`"     | tee    ${OUT}/start-stop.log
-echo "${BUILD} start: `date`"     | tee    ${OUT}/${BUILD}.log
+echo "${BUILD} start: `date --iso-8601=seconds`"     | tee    ${OUT}/start-stop.log
+echo "${BUILD} start: `date --iso-8601=seconds`"     | tee    ${OUT}/${BUILD}.log
 /usr/bin/time ./${BUILD}.sh 2>&1  | tee -a ${OUT}/${BUILD}.log
-echo "${BUILD} stop:  `date`"     | tee -a ${OUT}/${BUILD}.log
-echo "${BUILD} stop:  `date`"     | tee -a ${OUT}/start-stop.log
+echo "${BUILD} stop:  `date --iso-8601=seconds`"     | tee -a ${OUT}/${BUILD}.log
+echo "${BUILD} stop:  `date --iso-8601=seconds`"     | tee -a ${OUT}/start-stop.log
 
 for BUILD in 2-build-test 3-fetch 4-build-all-cores 5-build-8-bb-threads 6-build-16-bb-threads 7-build-2-bb-threads; do
   rm -rf poky/build/bitbake-cookerdaemon.log poky/build/cache poky/build/sstate-cache poky/build/tmp
-  echo "${BUILD} start: `date`"    | tee -a ${OUT}/start-stop.log
-  echo "${BUILD} start: `date`"    | tee    ${OUT}/${BUILD}.log
+  echo "${BUILD} start: `date --iso-8601=seconds`"    | tee -a ${OUT}/start-stop.log
+  echo "${BUILD} start: `date --iso-8601=seconds`"    | tee    ${OUT}/${BUILD}.log
   /usr/bin/time ./${BUILD}.sh 2>&1 | tee -a ${OUT}/${BUILD}.log
-  echo "${BUILD} stop:  `date`"    | tee -a ${OUT}/${BUILD}.log
-  echo "${BUILD} stop:  `date`"    | tee -a ${OUT}/start-stop.log
+  echo "${BUILD} stop:  `date --iso-8601=seconds`"    | tee -a ${OUT}/${BUILD}.log
+  echo "${BUILD} stop:  `date --iso-8601=seconds`"    | tee -a ${OUT}/start-stop.log
   grep -R "Elapsed time" poky/build/tmp/buildstats | sed 's/^.*\/\([^\/]*\/[^\/]*\):Elapsed time: \(.*\)$/\2 \1/g' | sort -n | tail -n 20 | tee ${OUT}/${BUILD}.top20
   mv poky/build/tmp/buildstats ${OUT}/buildstats-${BUILD}
 done
 
-tail -n 5 *.log
+tail -n 5 ${OUT}/*.log
