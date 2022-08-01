4bcaf07b881c16a3b1f5a5ff671d63ae2bd63e1b
Martin Jansa <Martin.Jansa@gmail.com>
2022-08-01 11:47:18 +0200

    test.sh: create logs in subdirectory which can be specified
    * makes a bit easier to keep the logs separate when running
      many tests in different branches

--- test.sh
+++ test.sh
@@ -2,21 +2,36 @@
 
 export LC_ALL=C
 
+OUT="${1}"
+
+if [ -z "${OUT}" ] ; then
+  BRANCH=`git branch --show-current`
+  OUT="results-${BRANCH}"
+fi
+
+if [ -d "${OUT}" ] ; then
+  echo "ERROR: output directory ${OUT} already exists, aborting to prevent accidentally overwitting the logs there"
+  echo "ERROR: delete this directory or set different one with first parameter"
+  exit 1
+else
+  mkdir ${OUT}
+fi
+
 BUILD=1-init
-echo "${BUILD} start: `date`"    | tee    start-stop.log
-echo "${BUILD} start: `date`"    | tee    ${BUILD}.log
-/usr/bin/time ./${BUILD}.sh 2>&1 | tee -a ${BUILD}.log
-echo "${BUILD} stop: `date`"     | tee -a ${BUILD}.log
-echo "${BUILD} stop: `date`"     | tee -a start-stop.log
+echo "${BUILD} start: `date`"     | tee    ${OUT}/start-stop.log
+echo "${BUILD} start: `date`"     | tee    ${OUT}/${BUILD}.log
+/usr/bin/time ./${BUILD}.sh 2>&1  | tee -a ${OUT}/${BUILD}.log
+echo "${BUILD} stop:  `date`"     | tee -a ${OUT}/${BUILD}.log
+echo "${BUILD} stop:  `date`"     | tee -a ${OUT}/start-stop.log
 
 for BUILD in 2-build-test 3-fetch 4-build-all-cores 5-build-8-bb-threads 6-build-16-bb-threads 7-build-2-bb-threads; do
   rm -rf poky/build/bitbake-cookerdaemon.log poky/build/cache poky/build/sstate-cache poky/build/tmp
-  echo "${BUILD} start: `date`"    | tee -a start-stop.log
-  echo "${BUILD} start: `date`"    | tee    ${BUILD}.log
-  /usr/bin/time ./${BUILD}.sh 2>&1 | tee -a ${BUILD}.log
-  echo "${BUILD} stop: `date`"     | tee -a ${BUILD}.log
-  echo "${BUILD} stop: `date`"     | tee -a start-stop.log
-  grep -R "Elapsed time" poky/build/tmp/buildstats | sed 's/^.*\/\([^\/]*\/[^\/]*\):Elapsed time: \(.*\)$/\2 \1/g' | sort -n | tail -n 20 | tee ${BUILD}.top20
+  echo "${BUILD} start: `date`"    | tee -a ${OUT}/start-stop.log
+  echo "${BUILD} start: `date`"    | tee    ${OUT}/${BUILD}.log
+  /usr/bin/time ./${BUILD}.sh 2>&1 | tee -a ${OUT}/${BUILD}.log
+  echo "${BUILD} stop:  `date`"    | tee -a ${OUT}/${BUILD}.log
+  echo "${BUILD} stop:  `date`"    | tee -a ${OUT}/start-stop.log
+  grep -R "Elapsed time" poky/build/tmp/buildstats | sed 's/^.*\/\([^\/]*\/[^\/]*\):Elapsed time: \(.*\)$/\2 \1/g' | sort -n | tail -n 20 | tee ${OUT}/${BUILD}.top20
 done
 
 tail -n 5 *.log
