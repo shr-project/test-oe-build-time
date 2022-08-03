4b8399fd224cdf0b24b6393bbd305a0061fc8cdf
Martin Jansa <Martin.Jansa@gmail.com>
2022-08-03 10:32:32 +0200

    [4567]-build*.sh: add -l ${@oe.utils.cpu_count()} in PARALLEL_MAKE
    * lets see if it help prevent 40G swap usage spike on all-cores builds
      it will be interesting to compare /proc/pressure data and grafana
      stats
    
    * autobuilder is already using that since:
      https://git.yoctoproject.org/yocto-autobuilder-helper/commit/?id=5c5fc7bcd221427d34bbac80c9bad315fb6de4df
    
    Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

--- 5-build-8-bb-threads.sh
+++ 5-build-8-bb-threads.sh
@@ -4,7 +4,7 @@ cd poky
 . ./oe-init-build-env
 cat >> conf/local.conf << EOF
 BB_NUMBER_THREADS = "8"
-PARALLEL_MAKE = "-j \${@oe.utils.cpu_count()}"
+PARALLEL_MAKE = "-j \${@oe.utils.cpu_count()} -l \${@oe.utils.cpu_count()}"
 EOF
 
 rm -rf bitbake-cookerdaemon.log cache sstate-cache tmp
