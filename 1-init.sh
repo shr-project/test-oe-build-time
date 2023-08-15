ab89983c02c631562cfceea2fa918f39e5f68f12
Martin Jansa <Martin.Jansa@gmail.com>
2023-08-15 13:25:28 +0200

    1-init.sh: update for latest revisions for mickledore
    Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

--- 1-init.sh
+++ 1-init.sh
@@ -6,26 +6,33 @@
 
 git clone git://git.yoctoproject.org/poky
 cd poky
-git checkout -b langdale b9223d27bdebd1356b3ece4c8735f561c947d47d
+git checkout -b mickledore 0a98281d37b0bd8dc1548c390216cb3978e36e0e
+git am ../0001-runqueue-show-more-pressure-data.patch
+
 git clone https://github.com/meta-qt5/meta-qt5.git
 cd meta-qt5
-git checkout -b langdale d466c12ed432f6d811dac6a80528a637ef5c658d
+git checkout -b mickledore d86e2e82c15d186141713515dfd1373013aaebd5
 cd ..
+
 git clone https://github.com/OSSystems/meta-browser.git
 cd meta-browser
-git checkout -b langdale 1f3ccca5678d7cbb221645227ec403aa767c16dd
+git checkout -b mickledore 1f3ccca5678d7cbb221645227ec403aa767c16dd
 cd ..
+
 git clone https://github.com/openembedded/meta-openembedded.git
 cd meta-openembedded
-git checkout -b langdale 2eb39477a784673baefa59640909b827ebb689b2
+git checkout -b mickledore 75cf318cef3b4ee81fad2782cf063ecd69ba8842
 cd ..
+
 git clone https://github.com/kraj/meta-clang.git
 cd meta-clang
-git checkout -b langdale da301d7c6627a5a1ee541a646aba22dc993109d9
+git checkout -b mickledore af4dcba009ba98250315520f3003fde4ee164cce
 cd ..
+
 git clone git://git.openembedded.org/meta-python2
 cd meta-python2
-git checkout -b langdale f02882e2aa9279ca7becca8d0cedbffe88b5a253
+git checkout -b mickledore f02882e2aa9279ca7becca8d0cedbffe88b5a253
+echo 'LAYERSERIES_COMPAT_meta-python2 = "mickledore"' >> conf/layer.conf
 cd ..
 
 . ./oe-init-build-env
