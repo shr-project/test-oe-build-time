78f352a185acb12bbe2ea85dac43f899ab80756c
Martin Jansa <Martin.Jansa@gmail.com>
2023-09-02 12:35:42 +0200

    [4567]-build*.sh: try to add BB_PRESSURE_MAX_CPU = 100 000
    * looks like after the PSI check logic changes from:
      https://git.openembedded.org/bitbake/commit/?id=95fa8fb5fb4d5a72e79b11d69792613bfd494e72
      https://git.openembedded.org/bitbake/commit/?id=b4763c2c93e7494e0a27f5970c19c1aac66c228b
      the 1K BB_PRESSURE_MAX_CPU value which worked resonably
      well in kirkstone (without these bitbake changes) degrades
      the all-cores, 8-bb-threads, 16-bb-threads times to almost 2-bb-threads
    
    												64	8	16	2
    threadripper-3970x-128gb-gentoo-kirkstone-2023-08-with-reg-cpu-pressure-100K-chp-logic-fixes	2:15:52	2:19:01	2:12:11	3:54:12
    threadripper-3970x-128gb-gentoo-kirkstone-2023-08-with-reg-cpu-pressure-100K			2:16:15	2:17:08	2:12:20	3:52:46
    threadripper-3970x-128gb-gentoo-kirkstone-2023-08-with-reg-cpu-pressure-1K			2:18:12	2:26:57	2:12:37	4:02:24
    threadripper-3970x-128gb-gentoo-nanbield-2023-08-no-ff						4:34:41	4:39:25	4:17:30	5:42:23
    
      to confirm this I'll also add
      threadripper-3970x-128gb-gentoo-kirkstone-2023-08-with-reg-cpu-pressure-1K-chp-logic-fixes
      and
      threadripper-3970x-128gb-gentoo-nanbield-2023-08-no-ff-with-reg-cpu-pressure-100K
    
      to confirm if logic-fixes break the performance in kirkstone and
      to see if increasing the pressure limit from 1K to 100K in nanbield
      fixes it (or if something else in nanbield makes it significantly slower
      while it should be a bit faster due to firefox removal from the image).
    
    Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

--- 5-build-8-bb-threads.sh
+++ 5-build-8-bb-threads.sh
@@ -5,7 +5,7 @@ cd poky
 cat >> conf/local.conf << EOF
 BB_NUMBER_THREADS = "8"
 PARALLEL_MAKE = "-j \${@oe.utils.cpu_count()} -l \${@oe.utils.cpu_count()}"
-BB_PRESSURE_MAX_CPU = "1000"
+BB_PRESSURE_MAX_CPU = "100000"
 EOF
 
 rm -rf bitbake-cookerdaemon.log cache sstate-cache tmp
--- 7-build-2-bb-threads.sh
+++ 7-build-2-bb-threads.sh
@@ -8,7 +8,7 @@ cd poky
 cat >> conf/local.conf << EOF
 BB_NUMBER_THREADS = "2"
 PARALLEL_MAKE = "-j \${@oe.utils.cpu_count()} -l \${@oe.utils.cpu_count()}"
-BB_PRESSURE_MAX_CPU = "1000"
+BB_PRESSURE_MAX_CPU = "100000"
 EOF
 
 rm -rf bitbake-cookerdaemon.log cache sstate-cache tmp
