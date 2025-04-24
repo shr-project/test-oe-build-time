651675d193a6c290f49e0dc35af2a7f8bba4512f
Martin Jansa <martin.jansa@gmail.com>
2025-04-24 13:04:50 +0200

    1-init.sh: cherry-pick few fixes for newer host OS
    Signed-off-by: Martin Jansa <martin.jansa@gmail.com>

--- 1-init.sh
+++ 1-init.sh
@@ -8,7 +8,26 @@ git clone git://git.yoctoproject.org/poky
 cd poky
 git checkout -b scarthgap 83793ccd865e3e72563e6d5733b6bd02943feb8a
 # to fix build on host with gcc-14
+# 4a1234df65 db: ignore implicit-int and implicit-function-declaration issues fatal with gcc-14
+# 818fe3d7df yocto-uninative: Update to 4.5 for gcc 14
 git cherry-pick 31b4ec9fd6 b77236fdeb
+# to fix build on host with python-13
+# 6741c78d3f ninja: fix build with python 3.13
+# 679b1842bb pseudo: Fix envp bug and add posix_spawn wrapper
+# d672cd4c93 pseudo: Update to include open symlink handling bugfix
+# f2e9c85eca pseudo: Fix to work with glibc 2.40
+# 2d5281492d pseudo: Update to pull in python 3.12+ fix
+git cherry-pick 6741c78d3f 2d5281492d f2e9c85eca d672cd4c93 679b1842bb
+# to fix build on host with glibc >= 2.41
+# d59b8312e9 qemu: Do not define sched_attr with glibc >= 2.41
+# other just for cherry-pick to apply cleanly:
+# 2775596cb2 qemu: upgrade 8.2.3 -> 8.2.7
+# dc5dd6ec19 qemu: back port patches to fix riscv64 build failure
+# 0069bab748 qemu: fix CVE-2024-7409
+# 8c533e9242 qemu: fix CVE-2024-4467
+# aa02ad000d qemu: upgrade 8.2.2 -> 8.2.3
+# 47789523dd qemu: Upgrade 8.2.1 -> 8.2.2
+git cherry-pick 47789523dd aa02ad000d 8c533e9242 0069bab748 dc5dd6ec19 2775596cb2 d59b8312e9
 
 git clone https://github.com/meta-qt5/meta-qt5.git
 cd meta-qt5
