f05677654c3d14a8ee333ac05ce8db902e0bd70e
Martin Jansa <martin.jansa@gmail.com>
2025-04-24 17:44:44 +0200

    1-init.sh: add scarthgap/rust mixing to build chromium-x11 and firefox
    * firefox needs newer as well:
    |  0:22.09 checking for rustc... /OE/build/test-oe-build-time/poky/build/tmp/work/core2-64-poky-linux/firefox/137.0.2/recipe-sysroot-native/usr/bin/rustc                                                                                     |  0:22.09 checking for cargo... /OE/build/test-oe-build-time/poky/build/tmp/work/core2-64-poky-linux/firefox/137.0.2/recipe-sysroot-native/usr/bin/cargo                                                                                     |  0:22.18 checking rustc version... 1.75.0                                                                                                                                                                                                   |  0:22.20 checking cargo version... 1.75.0                                                                                                                                                                                                   |  0:22.20 ERROR: Rust compiler 1.75.0 is too old.                                                                                                                                                                                            |  0:22.20                                                                                                                                                                                                                                    |  0:22.20 To compile Rust language sources please install at least                                                                                                                                                                           |  0:22.20 version 1.82.0 of the 'rustc' toolchain (or, if using nightly,                                                                                                                                                                     |  0:22.20 at least one version newer than 1.82.0) and make sure it is                                                                                                                                                                        |  0:22.20 first in your path.
    
    Signed-off-by: Martin Jansa <martin.jansa@gmail.com>

--- 1-init.sh
+++ 1-init.sh
@@ -19,6 +19,18 @@ cd meta-browser
 git checkout -b scarthgap a5109f054f9269e88225fe15b02174026574712b
 cd ..
 
+# Newer chromium uses -Zexternal-clangrt since:
+# https://chromium.googlesource.com/chromium/src/+/7d029cad43d3bf03b0464d9e3c55cc0f8b5474e2
+# and rust-1.75.0 in oe-core/scarthgap doesn't support it yet:
+# error: unknown unstable option: `external-clangrt`
+# it was adde din 1.78.0 with:
+# https://github.com/rust-lang/rust/pull/121207
+# use 1.85.1 from scarthgap/rust branch
+git clone https://git.yoctoproject.org/meta-lts-mixins
+cd meta-lts-mixins
+git checkout -b scarthgap 65b35821c8e39b13b20f0413c315d0c8f1a64a11
+cd ..
+
 git clone https://github.com/openembedded/meta-openembedded.git
 cd meta-openembedded
 git checkout -b scarthgap e92d0173a80ea7592c866618ef5293203c50544c
@@ -42,6 +54,7 @@ fi
 if ! grep -q meta-browser conf/bblayers.conf ; then
   sed -i 's/^\(.*\)meta-yocto-bsp/\1meta-yocto-bsp \\\n\1meta-browser\/meta-firefox/g' conf/bblayers.conf
   sed -i 's/^\(.*\)meta-yocto-bsp/\1meta-yocto-bsp \\\n\1meta-browser\/meta-chromium/g' conf/bblayers.conf
+  sed -i 's/^\(.*\)meta-yocto-bsp/\1meta-yocto-bsp \\\n\1meta-lts-mixins/g' conf/bblayers.conf
 fi
 
 cat >> conf/local.conf << EOF
